c63ebf41a2d1b4589aab1c225e44e987daab3cca
Natalie Beloeva <nbeloeva@gmail.com>
2021-10-03 14:44:37 +0300

    Adds component

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,14 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import MainScreen from '../main-screen/main-screen';
+
+type AppScreenProps = {
+  cardCount: number,
+  placeCount: number,
+}
+
+function App({cardCount, placeCount}: AppScreenProps): JSX.Element {
+  return (
+    <MainScreen cardCount={cardCount} placeCount={placeCount}/>
+  );
 }
 
 export default App;
